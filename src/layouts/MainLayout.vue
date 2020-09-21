4105d256e81279eb962690e4678b70770f517cee
ShaggyTech <beedat1@gmail.com>
2020-09-21 01:54:35 -0500

    layout: working on MainLayout.vue

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,42 +1,39 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
+  <q-layout view="lHh lpR fFr">
     <q-header elevated>
-      <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="leftDrawerOpen = !leftDrawerOpen"
-        />
-
-        <q-toolbar-title>
-          Quasar App
+      <q-toolbar class="text-white fit row items-center justify-between">
+        <div class="self-start q-my-auto">
+          <q-btn
+            stretch
+            flat
+            round
+            icon="menu"
+            aria-label="Menu"
+            @click="leftDrawerOpen = !leftDrawerOpen"
+          />
+          <q-separator inset size="4px" vertical />
+        </div>
+        <q-toolbar-title class="column items-center justify-center q-mx-md">
+          <q-icon name="img:icons/smart-foreman-logo.svg" :size="iconSize"></q-icon>Smart Foreman
         </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
+        <div class="self-end q-my-auto">
+          <q-btn
+            stretch
+            flat
+            round
+            icon="menu"
+            aria-label="Menu"
+            @click="leftDrawerOpen = !leftDrawerOpen"
+          />
+          <q-separator inset size="4px" vertical />
+        </div>
       </q-toolbar>
     </q-header>
 
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-      content-class="bg-grey-1"
-    >
+    <q-drawer v-model="leftDrawerOpen" show-if-above bordered content-class="bg-grey-1">
       <q-list>
-        <q-item-label
-          header
-          class="text-grey-8"
-        >
-          Essential Links
-        </q-item-label>
-        <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
-        />
+        <q-item-label header class="text-grey-8">Essential Links</q-item-label>
+        <EssentialLink v-for="link in essentialLinks" :key="link.title" v-bind="link" />
       </q-list>
     </q-drawer>
 
@@ -47,63 +44,94 @@
 </template>
 
 <script lang="ts">
-import EssentialLink from 'components/EssentialLink.vue'
+import EssentialLink from 'components/EssentialLink.vue';
 
 const linksData = [
   {
     title: 'Docs',
     caption: 'quasar.dev',
     icon: 'school',
-    link: 'https://quasar.dev'
+    link: 'https://quasar.dev',
   },
   {
     title: 'Github',
     caption: 'github.com/quasarframework',
     icon: 'code',
-    link: 'https://github.com/quasarframework'
+    link: 'https://github.com/quasarframework',
   },
   {
     title: 'Discord Chat Channel',
     caption: 'chat.quasar.dev',
     icon: 'chat',
-    link: 'https://chat.quasar.dev'
+    link: 'https://chat.quasar.dev',
   },
   {
     title: 'Forum',
     caption: 'forum.quasar.dev',
     icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
+    link: 'https://forum.quasar.dev',
   },
   {
     title: 'Twitter',
     caption: '@quasarframework',
     icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
+    link: 'https://twitter.quasar.dev',
   },
   {
     title: 'Facebook',
     caption: '@QuasarFramework',
     icon: 'public',
-    link: 'https://facebook.quasar.dev'
+    link: 'https://facebook.quasar.dev',
   },
   {
     title: 'Quasar Awesome',
     caption: 'Community Quasar projects',
     icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
+    link: 'https://awesome.quasar.dev',
+  },
 ];
 
-import { defineComponent, ref } from '@vue/composition-api';
+import { computed, defineComponent, ref, Ref } from '@vue/composition-api';
+
+// interface ViewPort {
+//   /* width of viewport (in px) */
+//   width: number;
+//   /* height of viewport (in px) */
+//   height: number;
+// }
+
+// type IconSize = '64px' | '96px' | '120px';
 
 export default defineComponent({
   name: 'MainLayout',
   components: { EssentialLink },
-  setup() {
-    const leftDrawerOpen = ref(false);
+  setup(_, { root: { $q } }) {
+    const leftDrawerOpen: Ref<boolean> = ref(false);
     const essentialLinks = ref(linksData);
+    // const viewport: ComputedRef<ViewPort> = computed(() => ({
+    //   height: $q.screen.height || 0,
+    //   width: $q.screen.width || 0,
+    // }));
+    const iconSize = computed(() => {
+      let size = 48;
+      if ($q.screen.height < 400) return `${36}px`;
+      switch ($q.screen.name) {
+        case 'sm':
+          size = 64;
+        case 'md':
+          size = 96;
+          break;
+        case 'lg':
+          size = 120;
+          break;
+        case 'xl':
+          size = 164;
+          break;
+      }
+      return `${size}px`;
+    });
 
-    return {leftDrawerOpen, essentialLinks}
-  }
+    return { iconSize, leftDrawerOpen, essentialLinks };
+  },
 });
 </script>
